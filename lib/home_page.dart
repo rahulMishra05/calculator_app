30ac276435232b3c11f93c8f38767105499f08c7
Rahul Mishra <rahulmishra102000@gmail.com>
2021-07-26 10:03:19 +0530

    Addition functionality added.

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -6,6 +6,20 @@ class HomePage extends StatefulWidget {
 }
 
 class HomePageState extends State<HomePage> {
+  var num1 = 0, num2 = 0, sum = 0;
+
+  final TextEditingController t1 = new TextEditingController(text: "0");
+  final TextEditingController t2 = new TextEditingController(text: "0");
+
+  void doAddition() {
+    setState(() {
+      num1 = int.parse(t1.text);
+      num2 = int.parse(t2.text);
+
+      sum = num1 + num2;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,43 +35,48 @@ class HomePageState extends State<HomePage> {
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               new Text(
-                "Output :",
+                "Output : $sum",
                 style: new TextStyle(
-                  fontSize: 25.0,
-                  color: Colors.blue,
-                  fontWeight: FontWeight.bold
-                ),
+                    fontSize: 25.0,
+                    color: Colors.blue,
+                    fontWeight: FontWeight.bold),
               ),
               new TextField(
                 keyboardType: TextInputType.number,
                 decoration: InputDecoration(hintText: "Enter number 1"),
+                controller: t1,
               ),
               new TextField(
                 keyboardType: TextInputType.number,
                 decoration: InputDecoration(hintText: "Enter number 2"),
+                controller: t2,
               ),
               new Padding(padding: const EdgeInsets.only(top: 20.0)),
               new Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: <Widget>[
-                  new MaterialButton(child: Text("+"), 
-                  color: Colors.greenAccent,
-                  onPressed: () {}),
-                  new MaterialButton(child: Text("-"), 
-                  color: Colors.greenAccent,
-                  onPressed: () {}),
+                  new MaterialButton(
+                      child: Text("+"),
+                      color: Colors.greenAccent,
+                      onPressed: doAddition),
+                  new MaterialButton(
+                      child: Text("-"),
+                      color: Colors.greenAccent,
+                      onPressed: () {}),
                 ],
               ),
               new Padding(padding: const EdgeInsets.only(top: 20.0)),
               new Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: <Widget>[
-                  new MaterialButton(child: Text("x"), 
-                  color: Colors.greenAccent,
-                  onPressed: () {}),
-                  new MaterialButton(child: Text("/"), 
-                  color: Colors.greenAccent,
-                  onPressed: () {}),
+                  new MaterialButton(
+                      child: Text("x"),
+                      color: Colors.greenAccent,
+                      onPressed: () {}),
+                  new MaterialButton(
+                      child: Text("/"),
+                      color: Colors.greenAccent,
+                      onPressed: () {}),
                 ],
               ),
             ],
